339f51fd436542c4f2e1d456b9aacf3592c351c9
AA3111s <77196802+AA3111s@users.noreply.github.com>
2026-06-06 23:08:24 +0530

    progress

---
+++ LuvCP/hash_ps/word_pattern.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string pattern;
+    string s;
+    cin >> pattern >> s;
+
+   if(pattern.size() != s.size()){
+        return false;
+   }
+   string hsh1[27];
+   string hsh2[3001];
+
+   for(int i = 0; i < pattern.size(); i++){
+     hsh1[pattern[i]] = s[i];
+     hsh2[s[i]] = pattern[i];
+     
+   }
+}
