04d958119857c54c15ef13e36edb553504536494
AA3111s <77196802+AA3111s@users.noreply.github.com>
2026-07-05 23:38:29 +0530

    stack

--- past_contests/precomputation_luvcp/d.cpp
+++ past_contests/precomputation_luvcp/d.cpp
@@ -1,6 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int N = 1e5 + 10;
+
 int main(){
+    int n, k;
+    cin >> n >> k;
     
+    int hsh[N] = {0};
+    int hsh2[N] = {0};
+    
+    for(int i = 0; i < n; i++){
+        int temp;
+        cin >> temp;
+        hsh2[temp]++;
+    }
+    
+    int counter = 0;
+    
+    for(int i = 0; i < N; i++){
+        if(hsh2[i] > k){
+            counter++;
+        }
+    }
+    
+    cout << counter;
 }
+
+
+
+/*
+sample test case
+10 2
+23 5 1 23 8 1 1 23 86 75
+
+expected output 
+2
+*/
--- past_contests/precomputation_luvcp/e.cpp
+++ past_contests/precomputation_luvcp/e.cpp
@@ -5,10 +5,25 @@ int main(){
     string s;
     cin >> s;
     
+    int hsh[28] = {0};
+
     int q;
     cin >> q;
     while(q--){
         int n;
         cin >> n;
+        for(int i = 0; i < n; i++){
+            char temp;
+            cin >> temp;
+            hsh[temp]++;
+        }
+        int evens = 0;
+        int odds = 0;
+        for(int i = 0; i < 27; i++){
+            if(hsh[i] % 2 == 0){
+                evens++;
+            } else odds++;
+        }
+        cout << (odds > 1) ? evens+1 : evens;
     }
 }
